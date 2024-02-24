9dfad88cb22328755339e5d2e5837d2436f8c484
Teckwips.com <157291388+FagbohunOre@users.noreply.github.com>
2024-02-24 14:08:27 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -328,15 +328,15 @@ function type() {
 
 // creating responsive navbar
 
-let openHam = document.querySelector('#openHam');
-let closeHam = document.querySelector('#closeHam');
-let navigationItems = document.querySelector('#navigation-items');
+// let openHam = document.querySelector('#openHam');
+// let closeHam = document.querySelector('#closeHam');
+// let navigationItems = document.querySelector('#navigation-items');
 
-const hamburgerEvent = (navigation, close, open) => {
-    navigationItems.style.display = navigation;
-    closeHam.style.display = close;
-    openHam.style.display = open;
-};
+// const hamburgerEvent = (navigation, close, open) => {
+//     navigationItems.style.display = navigation;
+//     closeHam.style.display = close;
+//     openHam.style.display = open;
+// };
 
-openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
-closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+// openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+// closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
